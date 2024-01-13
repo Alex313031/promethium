7bdaadd9c52af1ef7ecf8688456b4466996906a6
Alexander Frick <Alex313031@gmail.com>
2024-01-12 18:12:04 -0600

    restage 2

--- src/renderer/constants/imgs.ts
+++ src/renderer/constants/imgs.ts
@@ -1,9 +1,9 @@
 import { resolve, join } from 'path';
 
 // Background images for the New Tab Page
-export const BACKGROUND_IMAGE = require('~/assets/backgrounds/Dawn.gif');
-export const BACKGROUND_IMAGE2 = require('~/assets/backgrounds/Girls_Just_Wanna_Have_Fun.png');
-export const BACKGROUND_IMAGE3 = require('~/assets/backgrounds/Carl_Kleiner_Dark_Material.jpg');
+export const BACKGROUND_IMAGE = require('~/assets/backgrounds/Carl_Kleiner_Dark_Material.jpg');
+export const BACKGROUND_IMAGE2 = require('~/assets/backgrounds/Carl_Kleiner_Papercscape.jpg');
+export const BACKGROUND_IMAGE3 = require('~/assets/backgrounds/Carl_Kleiner_Hommage_to_Magritte.jpg');
 
 // Main app icon
 export const APP_ICON = require('~/assets/imgs/app_icon.png');
